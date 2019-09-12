d0c607e19a2fce949c6973e6d755db978b3c336b
Garrett Thompson <gtcruz.gt@gmail.com>
2019-09-12 03:53:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 class Dog
 
-  attr_accessor :mood, :owner
+  attr_accessor :mood, :owner, :mydog
   attr_reader :name
 
   @@dog = []
+  @mydog = []
 
   def initialize(name, owner="nil")
     @name = name
     @owner = owner
     @mood = "nervous"
     @@dog << self
+    @mydog << self
   end
 
   def self.all
